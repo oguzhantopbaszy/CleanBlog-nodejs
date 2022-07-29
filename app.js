2983382920f1d2a5fbf44400be73632a9587a823
oguzhantopbaszy <oguzhantopbas@zyelektrikli.com>
2022-07-29 16:10:25 +0300

    Stack files and Partial confiğurations completed

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require('express')
 
+const ejs = require('ejs')
+const path = require('path')
+
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.static('public'))
 
-    const blog = {
-        id:1,
-        title: "Blog Title",
-        description: "Blog Description"
-    }
-    res.send(blog)
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index')
 })
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/post', (req,res) => {
+    res.render('post')
+})
+
+
 
 const port = 3000;
 app.listen(port, () => {
