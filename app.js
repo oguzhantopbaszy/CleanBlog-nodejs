dcaf62f9eaadae02a4dedcb87e24d0170398bc6c
oguzhantopbaszy <oguzhantopbas@zyelektrikli.com>
2022-08-02 09:59:28 +0300

    HW3 Post creating completed

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const ejs = require('ejs')
 const path = require('path')
+const Post = require('./models/Post')
 
 const app = express();
 
 app.use(express.static('public'))
 
+//Connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 //Template Engine
 app.set("view engine", "ejs");
 
 //Middlewares
-app.use(express.static('public'));
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const posts = await Post.find({})
+    res.render('index', {
+        posts
+    })
 })
 
 app.get('/about', (req, res) => {
@@ -23,6 +36,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add_post', (req, res) => {
     res.render('add_post')
+})
+
+app.post('/add_post', async (req, res) => {
+    await Post.create(req.body)
+    res.redirect('/')
 })
 
 app.get('/about', (req, res) => {
